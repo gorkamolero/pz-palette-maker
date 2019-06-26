38570552beb30aedb7d19d2a6f1058beee94f366
Gorka <gorka.molero@gmail.com>
2019-06-26 15:27:12 +0200

    Moving to vue widget

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+import vueCustomElement from 'vue-custom-element'
+Vue.use(vueCustomElement)
+Vue.customElement('pz-palette-maker', App)
